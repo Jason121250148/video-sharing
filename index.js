48852d396e5d28d61d371bee26c1cd00b6c7cfeb
Jason121250148 <962215093@qq.com>
2017-02-13 10:42:08 +0800

    Change port to 8081.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import app from './src/server/app';
 
 const httpServer = http.createServer(app);
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 0) : 8080;
+const port = process.env.PORT ? parseInt(process.env.PORT, 0) : 8081;
 httpServer.listen(port, () => {
     console.log(`The video sharing server is now running at HTTP ${port}.`);
 });

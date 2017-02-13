04d1db5dad53c3ce0c370ae3fbab0de7c6af6006
Jason121250148 <962215093@qq.com>
2017-02-13 10:42:49 +0800

    Show welcome.

--- src/server/routes/project.js
+++ src/server/routes/project.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/app', (req, res) => {
-    res.render('project/project-app');
+    res.render('project/welcome');
 });
 
 export default router;

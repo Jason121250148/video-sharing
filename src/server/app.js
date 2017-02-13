b130ff0f8698a54f9d8e884bdcb69274ec7dfef8
Jason121250148 <962215093@qq.com>
2017-02-13 10:42:20 +0800

    Fix bug.

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,12 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import path from "path";
 
-const app = server();
+const app = express();
+
+// Set Pug as the default view engine.
+app.set('view engine', 'pug');
+app.set('views', path.resolve(__dirname, './views'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend: false }));

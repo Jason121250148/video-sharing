f949dc90444b1834d81b27dcfe8fdef2281d68c3
Jason121250148 <962215093@qq.com>
2017-02-13 15:31:39 +0800

    Add urlRre.

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,12 @@
 import bodyParser from 'body-parser';
+import config from 'config';
 import express from 'express';
 import path from "path";
 
 const app = express();
+
+// Initialize app settings.
+app.set('assets url prefix', config.get('web.assets.urlPrefix'));
 
 // Set Pug as the default view engine.
 app.set('view engine', 'pug');
@@ -10,6 +14,11 @@ app.set('views', path.resolve(__dirname, './views'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend: false }));
+
+app.getAssetUrl = function getAssetUrl(relPath) {
+  const prefix = app.get('assets url prefix');
+  return `${prefix}${relPath}`;
+};
 
 app.use('/', require('./routes').default);
 
